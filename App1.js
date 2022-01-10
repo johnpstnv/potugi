3b64be4d0401ada61681a7d42f480d644e17ebe1
johnpstnv <john.postnov@gmail.com>
2022-01-10 19:45:53 +0300

    second commit

--- App1.js
+++ App1.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vk.com/certainties"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Listen Certainties
         </a>
       </header>
     </div>
